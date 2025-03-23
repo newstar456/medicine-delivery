246b4dafd9af8f9cd289c39a6480cb5776616a49
Viktoria Vonsovych <vikvons@gmail.com>
2025-03-23 22:10:56 +0100

    initial commit

---
+++ server/index.mjs
@@ -0,0 +1,66 @@
+// const express = require ('express')
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = express();
+// app.use(cors())
+
+
+app.use(cors({
+  origin: ["http://localhost:3000", 'https://vercel-deployment-client-bice.vercel.app'], 
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+}));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.options('*', cors()); 
+
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://eyxccahssmwxluobjzyb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5eGNjYWhzc213eGx1b2JqenliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MTE3NDYsImV4cCI6MjA1Nzk4Nzc0Nn0.EyAezK8taMRdmnGdvmQFSI2wtbEhFqlsBjE8n4NQIu4')
+
+app.get('/', (req, res) => {
+  async function fetchShops() { 
+    try {
+      const {data, error} = await supabase.from('Meds').select('id, name, price, available_qty, shop_id, img, favorite, Shops(id,name, adress, title)');
+      res.status(200).json(JSON.stringify(data, null, 2));
+      console.log('Connection to SUPABASE has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  } 
+  fetchShops();
+});
+
+app.post('/', async (req, res) => {
+  try {
+    const {data, error} = await supabase.from('Carts').select();
+    // res.status(200).json(JSON.stringify({hello:'hello'}, null, 2));
+    const itemExists = await data.filter((item) => item.name === req.body.name&&item.shop===req.body['Shops'].id);
+    if(itemExists.length > 0) {
+      const {quantity} = itemExists;
+      const newQty = Number(quantity) + Number(req.body.quantity);
+      // await supabase.from('Carts').update({ 'quantity': `${newQty}` }).eq('id', `${itemExists.id}`);
+      res.status(200).json(JSON.stringify(quantity, null, 2));
+    } else {
+      const idInCart = `${req.body.id}${new Date().getTime()}`
+      const newItem =  { id: idInCart, name: req.body.name, shop: req.body.shop_id, quantity:req.body.quantity, price: req.body.price};
+      await supabase.from('Carts').insert({'id': `${req.body.id}`, 'name': `${req.body.name}`, 'shop':`${req.body.shop_id}`, 'quantity':`${req.body.quantity}`, 'price':`${req.body.price}`});
+      res.status(200).json(JSON.stringify(newItem, null, 2));
+    }
+    // res.status(200).json(JSON.stringify(itemExists, null, 2));
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// app.get('/home', (req, res) => {
+//   res.status(200).json('Welcome, your app is working well');
+// });
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(4000, console.log('Server has started on port 4000'))
