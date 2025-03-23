804960498b30e60dec7986195dc936316e28414a
Viktoria Vonsovych <vikvons@gmail.com>
2025-03-23 22:53:13 +0100

    home page

---
+++ client/src/components/Med.js
@@ -0,0 +1,122 @@
+'use client'
+import React, {useState} from 'react'
+// import {Box, Typography, Stack, Grid2, Button} from '@mui/material'
+// import { useTheme } from '@mui/material/styles'
+// import styled from "styled-components"
+// import {Buffer} from "buffer" 
+import axios from "axios"
+
+// const MedBox = styled(Box) (function () {
+//   const theme = useTheme();
+//   return {
+//     color:`${theme.palette.text.secondary}`,
+//     textAlign:"start",
+//    [theme.breakpoints.up("mobile")]: {
+//       // paddingBottom:'16px',
+//       fontSize:'12px'
+//     },
+//     [theme.breakpoints.up("laptop")]: {
+//       // margin:'15px',
+//       fontSize:'16px'
+//     }
+//   }
+// })
+// const ImageBox = styled(Box) (function () {
+//   const theme = useTheme();
+//   return {
+//     [theme.breakpoints.up("mobile")]: {
+//       height:'90px',
+//       width:'auto',
+//     },
+//     [theme.breakpoints.up("laptop")]: {
+//       height:'120px',
+//       width:'auto',
+//     },
+//     [theme.breakpoints.up("desktop")]: {
+//       height:'150px',
+//       width:'auto',
+//     }
+//   }
+// })
+// const QtyButtons = styled(Button) (function () {
+//   const theme = useTheme();
+//   return {
+//       color:`${theme.palette.text.secondary}`,
+//       fontSize:'16px',
+//       '&:hover': {
+//           fontWeight: 'bold',
+//           backgroundColor: 'transparent',
+//       }
+//   }
+// })
+// const QtyStack = styled(Stack) (function () {
+//   const theme = useTheme();
+//   return {
+//     color:`${theme.palette.text.secondary}`,
+//     fontSize:'16px',
+//     [theme.breakpoints.up("phone")]: {
+//       display:'none'
+//     },
+//     [theme.breakpoints.up("tablet")]: {
+//       display:'block'
+//     },
+//   }
+// })
+
+const Med = ({med}) => {
+  
+  const theme = useTheme();
+
+  let startingQty = 1;
+  const [quantity, setQuantity] = useState(startingQty)
+  const qtyDecrease = () => {
+    if (quantity === 1) return;
+    setQuantity(quantity - 1)
+  }
+  const qtyIncrease = () => setQuantity(quantity + 1)
+
+
+
+const addToCart = async () => {
+  const medToAdd = {...med, quantity:quantity}
+  try {
+    const response = await axios.post(
+      "https://vercel-deployment-five-xi.vercel.app", 
+      medToAdd, 
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        withCredentials: true 
+      }
+    );
+    // console.log(JSON.parse(response.config.data));
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+  // console.log(med);
+  return (
+    <div size={5} sx={{ border: ` 1px solid ${theme.palette.text.secondary}`, borderRadius:'4px'}}>
+      <div justifyContent="center" alignItems='center'> <div component="img" src={`${med.img}`}/></div>
+      <div direction='row' sx={{justifyContent: "space-between", alignItems: "center"}}>
+        <div>{med.name}</div>
+        <div direction='row' justifyContent='center' alignItems='center'>
+          <div onClick={qtyDecrease}>{'<'}</div>
+          <div sx={{fontStyle:`bold`}}>{quantity}</div>
+          <div onClick={qtyIncrease}>{'>'}</div>
+        </div>
+        <div>{`price: ${med.price} грн`}</div>
+      </div>
+      <div container spacing={0} justifyContent="right" alignItems='center'><button variant='outlined' onClick={addToCart}>add to Cart</button></div>
+      </div>
+    
+  )
+}
+
+export default Med
+
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Link from 'next/link'
+// import { useRouter } from 'next/router'
+import {LinkedInIcon, TwitterIcon, GithubIcon, DribbbleIcon, PinterestIcon} from './Icons'
+import { motion } from 'framer-motion'
+
+const CustomLink = ({href, title, className=""}) => {
+  // const router = useRouter();
+  return(
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300`}>&nbsp;</span>
+    </Link>
+  )
+}
+
+const NavBar = () => {
+  return (
+    <header className='w-full px-32 py-8 font-medium flex item-center justify-between'>
+    
+      <nav>
+        <CustomLink href='/' title="SHOPS" className='mr-4'/>
+        <CustomLink href='/cart' title='SHOPPING CART' className='mx-4'/>
+      </nav>
+
+
+    </header>
+  )
+}
+
+export default NavBar
